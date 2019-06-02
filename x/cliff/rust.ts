082cb060db5f9f7621fe09f808a31b51af679e15
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-02 17:30:12 -0400

    await more stuff

--- x/cliff/rust.ts
+++ x/cliff/rust.ts
@@ -6,17 +6,18 @@ export namespace Rustup {
             args: ["curl", "https://sh.rustup.rs", "-sSf"],
             stdout: "piped"
         };
-        let p1 = Deno.run(opts1);
+        let p1 = await Deno.run(opts1);
         let opts2: Deno.RunOptions = { 
             args: ["sh", "-s", "--", "-y", "--default-toolchain", "nightly"],
             stdin: "piped"
         };
-        let p2 = Deno.run(opts2);
+        let p2 = await Deno.run(opts2);
         await Deno.copy(p2.stdin, p1.stdout)
     }
 }
 
 export namespace Cargo {
+    // Build takes a full-qualified path to a Cargo.toml
     export async function build(path: string): Promise<Deno.Process> {
         let opts: Deno.RunOptions = { args: ["cargo", "build", "--manifest-path", path]}
         return Deno.run(opts);
