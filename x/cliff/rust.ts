644d9f4b4e25b31787a051e933b0092e9db8bf72
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-02 16:47:07 -0400

    rustup install wrapper

---
+++ x/cliff/rust.ts
@@ -0,0 +1,17 @@
+export namespace Rustup {
+    export async function install(): Promise<void> {
+
+        // "curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain \"nightly\""
+        let opts1: Deno.RunOptions = { 
+            args: ["curl", "https://sh.rustup.rs", "-sSf"],
+            stdout: "piped"
+        };
+        let p1 = Deno.run(opts1);
+        let opts2: Deno.RunOptions = { 
+            args: ["sh", "-s", "--", "-y", "--default-toolchain", "nightly"],
+            stdin: "piped"
+        };
+        let p2 = Deno.run(opts2);
+        await Deno.copy(p2.stdin, p1.stdout)
+    }
+}
