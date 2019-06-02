843f6c826ff22e1bc9905cc55c42027516f7f1eb
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-02 00:47:13 -0400

    try to export from mod

--- x/roles/base.ts
+++ x/roles/base.ts
@@ -1,7 +1,10 @@
 
-import { User, getUsers } from "https://deno.coleman.codes/x/cliff/user.ts"
-import { GitRepo } from "https://deno.coleman.codes/x/cliff/git.ts"
+// import { User, getUsers } from "https://deno.coleman.codes/x/cliff/user.ts"
+// import { GitRepo } from "https://deno.coleman.codes/x/cliff/git.ts"
+import { User, getUsers, GitRepo } from "https://deno.coleman.codes/x/cliff/mod.ts"
 
+const redoxDeps = ["cmake", "make", "nasm", "qemu", "pkg-config", "libfuse-dev", 
+  "wget", "gperf", "libhtml-parser-perl", "autoconf", "flex autogen"]
 
 async function main(): Promise<void> {
   const home = Deno.env()["HOME"];
