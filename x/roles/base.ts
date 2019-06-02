e5fd759e35619769e2bb0b39c455c4585a00dbdb
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-02 00:07:20 -0400

    clone redox

--- x/roles/base.ts
+++ x/roles/base.ts
@@ -5,7 +5,9 @@ import { GitRepo } from "https://deno.coleman.codes/x/cliff/git.ts"
 
 async function main(): Promise<void> {
   const home = Deno.env()["HOME"];
-  let r = await new GitRepo("git@github.com:spacejam/sled.git").clone();
+  await new GitRepo("git@github.com:spacejam/sled.git").clone();
+  await new GitRepo("https://gitlab.redox-os.org/redox-os/redox.git").clone({ recurseSubmodules: true });
+
 }
 
 main();
