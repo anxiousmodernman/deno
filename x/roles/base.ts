33c03c94f28957987828559362fa0b763cb6f8a0
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-02 16:49:36 -0400

    rustup install in base role

--- x/roles/base.ts
+++ x/roles/base.ts
@@ -1,8 +1,11 @@
 import { User, getUsers, GitRepo, Package } from "https://deno.coleman.codes/x/cliff/mod.ts"
+import { Rustup } from "https://deno.coleman.codes/x/cliff/rust.ts"
 
 async function main(): Promise<void> {
   const home = Deno.env()["HOME"];
   Deno.chdir(home);
+
+  await Rustup.install();
 
   const redoxDeps = ["cmake", "make", "nasm", "qemu", "pkg-config", "libfuse-dev", 
     "wget", "gperf", "libhtml-parser-perl", "autoconf", "flex", "autogen"];
