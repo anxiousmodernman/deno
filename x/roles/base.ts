f44c872676af7aa408fb37e0b4ceee89213452a6
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-01 23:47:47 -0400

    can we clone

--- x/roles/base.ts
+++ x/roles/base.ts
@@ -3,15 +3,9 @@ import { User, getUsers } from "https://deno.coleman.codes/x/cliff/user.ts"
 import { GitRepo } from "https://deno.coleman.codes/x/cliff/git.ts"
 
 
-function gitClone(repo: string): Promise<void> {
-  let result = await Deno.run({ })
-}
-
-
 async function main(): Promise<void> {
-  let u = new User("coleman");
-  
-  Deno.run({ "args": ["ls"]});
+  const home = Deno.env()["HOME"];
+  let r = await GitRepo("git@github.com:spacejam/sled.git").clone();
 }
 
 main();
