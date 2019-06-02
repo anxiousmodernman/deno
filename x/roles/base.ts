812f6bd62761ef056a4ee708ad5626f402602bdc
Coleman McFarland <coleman.mcfarland@gmail.com>
2019-06-02 00:16:52 -0400

    warn that redox is a lot of data on disk

--- x/roles/base.ts
+++ x/roles/base.ts
@@ -6,6 +6,7 @@ import { GitRepo } from "https://deno.coleman.codes/x/cliff/git.ts"
 async function main(): Promise<void> {
   const home = Deno.env()["HOME"];
   await new GitRepo("git@github.com:spacejam/sled.git").clone();
+  // careful, redox is like several gigs
   await new GitRepo("https://gitlab.redox-os.org/redox-os/redox.git").clone({ recurseSubmodules: true });
 
 }
