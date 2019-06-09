85fc753eaac35d8d81612837cf6f4b0ea77458e2
Coleman McFarland <cmcfarland@paperlesspost.com>
2019-06-09 09:33:57 -0400

    am i doing rustup right?

--- x/roles/rust.ts
+++ x/roles/rust.ts
@@ -1,6 +1,8 @@
 import { Rustup } from "../cliff/rust.ts"
 async function main() {
+    console.log("installing rust");
     let p = await Rustup.install();
+    console.log("DONE DONE DONE! WOOOO!")
 }
 
 main();
