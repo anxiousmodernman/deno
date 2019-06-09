e23236547e543ccb02be612c5f0d57d69b6be312
Coleman McFarland <cmcfarland@paperlesspost.com>
2019-06-09 10:28:49 -0400

    await things in a different way

--- x/cliff/rust.ts
+++ x/cliff/rust.ts
@@ -1,22 +1,19 @@
 export namespace Rustup {
-    export async function install(): Promise<[Deno.ProcessStatus, Deno.ProcessStatus, number]> {
+    export async function install(): Promise<[Deno.ProcessStatus, Deno.ProcessStatus]> {
 
         // "curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain \"nightly\""
         let opts1: Deno.RunOptions = { 
             args: ["curl", "https://sh.rustup.rs", "-sSf"],
             stdout: "piped",
-            stderr: null,
         };
         let p1 = Deno.run(opts1);
         let opts2: Deno.RunOptions = { 
             args: ["sh", "-s", "--", "-y", "--default-toolchain", "nightly"],
             stdin: "piped",
-            stdout: null, 
-            stderr: null,
         };
         let p2 = Deno.run(opts2);
-        let p3 = Deno.copy(p2.stdin, p1.stdout);
-        return Promise.all([p1.status(), p2.status(), p3])
+        Deno.copy(p2.stdin, p1.stdout);
+        return Promise.all([p1.status(), p2.status()]);
     }
 }
 
